5f9fff3492ba1aa4685f4a6d2bb4ac9897da2efe
ncitron <noah@jeff.org>
2021-11-08 18:20:14 -0500

    add test

--- src/BedOracle.sol
+++ src/BedOracle.sol
@@ -4,7 +4,11 @@ pragma solidity ^0.8.6;
 import { IChainlinkAggregatorV3 } from "./IChainlinkAggregatorV3.sol";
 import { ISetToken } from "./ISetToken.sol";
 
+import { SafeCast } from "@openzeppelin/contracts/utils/math/SafeCast.sol";
+
+
 contract BedOracle {
+    using SafeCast for int256;
     
     ISetToken public bed;
 
@@ -36,6 +40,22 @@ contract BedOracle {
     }
 
     function getPrice() external view returns (uint256) {
+        uint256 wbtcUnits = bed.getDefaultPositionRealUnit(wbtc).toUint256();
+        uint256 wethUnits = bed.getDefaultPositionRealUnit(weth).toUint256();
+        uint256 dpiUnits = bed.getDefaultPositionRealUnit(dpi).toUint256();
 
+        uint256 wbtcPrice = wbtcFeed.latestAnswer().toUint256();
+        uint256 dpiPrice = dpiFeed.latestAnswer().toUint256();
+
+        return _getValue(wbtcUnits, wbtcPrice, 8) + _getValue(dpiUnits, dpiPrice, 18) + wethUnits;
+    }
+
+    function _getValue(uint256 _units, uint256 _price, uint8 _decimals) internal pure returns (uint256) {
+        uint256 normalizedUnits = _units * 10 ** (18 - _decimals);
+        return _preciseMul(normalizedUnits, _price);
+    }
+
+    function _preciseMul(uint256 _a, uint256 _b) internal pure returns (uint256) {
+        return (_a * _b) / 1 ether;
     }
 }
--- src/BedOracle.t.sol
+++ src/BedOracle.t.sol
@@ -3,20 +3,67 @@ pragma solidity ^0.8.6;
 
 import "ds-test/test.sol";
 
-import "./BedOracle.sol";
+import { BedOracle } from "./BedOracle.sol";
+import { ChainlinkAggregatorV3Mock } from "./ChainlinkAggregatorV3Mock.sol";
+import { IChainlinkAggregatorV3 } from "./IChainlinkAggregatorV3.sol";
+import { SetTokenMock } from "./SetTokenMock.sol";
+
 
 contract BedOracleTest is DSTest {
+
     BedOracle oracle;
 
+    SetTokenMock bed;
+    
+    address wbtc;
+    address weth;
+    address dpi;
+
+    ChainlinkAggregatorV3Mock btcFeed;
+    ChainlinkAggregatorV3Mock dpiFeed;
+
     function setUp() public {
-        oracle = new BedOracle();
+        wbtc = address(0x1);
+        weth = address(0x2);
+        dpi = address(0x3);
+
+        address[] memory components = new address[](3);
+        components[0] = wbtc;
+        components[1] = weth;
+        components[2] = dpi;
+
+        int256[] memory units = new int256[](3);
+        units[0] = 1 * 10**5;   // 0.001 wBTC
+        units[1] = 0.01 ether;
+        units[2] = 0.1 ether;
+
+        bed = new SetTokenMock(components, units);
+
+        btcFeed = new ChainlinkAggregatorV3Mock();
+        btcFeed.setPrice(13 ether);
+
+        dpiFeed = new ChainlinkAggregatorV3Mock();
+        dpiFeed.setPrice(0.1 ether);
+
+        oracle = new BedOracle(
+            bed,
+            wbtc,
+            weth,
+            dpi,
+            IChainlinkAggregatorV3(address(btcFeed)),
+            IChainlinkAggregatorV3(address(dpiFeed))
+        );
     }
 
-    function testFail_basic_sanity() public {
-        assertTrue(false);
-    }
+    function test_getPrice() public {
+        uint256 price = oracle.getPrice();
 
-    function test_basic_sanity() public {
-        assertTrue(true);
+        uint256 wbtcValue = uint(bed.getDefaultPositionRealUnit(wbtc))*10**10 * uint(btcFeed.latestAnswer()) / 1 ether;
+        uint256 dpiValue = uint(bed.getDefaultPositionRealUnit(dpi)) * uint(dpiFeed.latestAnswer()) / 1 ether;
+        uint256 wethValue = uint(bed.getDefaultPositionRealUnit(weth));
+
+        uint256 expectedPrice = wbtcValue + dpiValue + wethValue;
+
+        assertEq(price, expectedPrice);
     }
 }
