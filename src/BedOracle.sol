37ff0545a38f81c2fa909df39f4a328c8650c1e6
ncitron <noah@jeff.org>
2021-11-09 00:28:10 -0500

    add natspec comments

--- src/BedOracle.sol
+++ src/BedOracle.sol
@@ -7,26 +7,49 @@ import { ISetToken } from "./interfaces/ISetToken.sol";
 import { SafeCast } from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 
 
+/// @title BedOracle
+/// @author ncitron
+/// @notice Price oracle for the BED token
+/// @dev This setup should not be replicated for other set tokens. This works for BED since
+/// it is guaranteed that BED will never contain any other tokens except for wBTC, WETH,
+/// and DPI (even during rebalancing).
 contract BedOracle {
     using SafeCast for int256;
     
+    /// @notice BED token
     ISetToken public bed;
 
-    // component addresses
+    /// @notice wBTC token address
     address public wbtc;
+
+    /// @notice WETH token address
     address public weth;
+
+    /// @notice DPI token address
     address public dpi;
 
-    // Chainlink feeds priced in ETH
-    IChainlinkAggregatorV3 public wbtcFeed;
+    /// @notice BTC Chainlink price feed
+    /// @dev Must be priced in ETH and return 18 decimals
+    IChainlinkAggregatorV3 public btcFeed;
+
+
+    /// @notice DPI Chainlink price feed
+    /// @dev Must be priced in ETH and return 18 decimals
     IChainlinkAggregatorV3 public dpiFeed;
 
+    /// @notice Creates a new BedOracle
+    /// @param _bed  BED token
+    /// @param _wbtc wBTC token
+    /// @param _weth WETH token
+    /// @param _dpi DPI token
+    /// @param _btcFeed Chainlink feed for BTC/ETH
+    /// @param _dpiFeed Chainlink feed for DPI/ETH
     constructor(
         ISetToken _bed,
         address _wbtc,
         address _weth,
         address _dpi,
-        IChainlinkAggregatorV3 _wbtcFeed,
+        IChainlinkAggregatorV3 _btcFeed,
         IChainlinkAggregatorV3 _dpiFeed
     ) {
         bed = _bed;
@@ -35,26 +58,33 @@ contract BedOracle {
         weth = _weth;
         dpi = _dpi;
         
-        wbtcFeed = _wbtcFeed;
+        btcFeed = _btcFeed;
         dpiFeed = _dpiFeed;
     }
 
+    /// @notice Gets the price of BED in ETH
+    /// @return BED price
     function getPrice() external view returns (uint256) {
         uint256 wbtcUnits = bed.getDefaultPositionRealUnit(wbtc).toUint256();
         uint256 wethUnits = bed.getDefaultPositionRealUnit(weth).toUint256();
         uint256 dpiUnits = bed.getDefaultPositionRealUnit(dpi).toUint256();
 
-        uint256 wbtcPrice = wbtcFeed.latestAnswer().toUint256();
+        uint256 wbtcPrice = btcFeed.latestAnswer().toUint256();
         uint256 dpiPrice = dpiFeed.latestAnswer().toUint256();
 
         return _getValue(wbtcUnits, wbtcPrice, 8) + _getValue(dpiUnits, dpiPrice, 18) + wethUnits;
     }
 
+    /// @dev Calculates the value of a set component in ETH
+    /// @param _units Amount of the component per set
+    /// @param _price Price of the component in ETH
+    /// @param _decimals Decimals of the component token
     function _getValue(uint256 _units, uint256 _price, uint8 _decimals) internal pure returns (uint256) {
         uint256 normalizedUnits = _units * 10 ** (18 - _decimals);
         return _preciseMul(normalizedUnits, _price);
     }
 
+    /// @dev multiplies two 18 decimal fixed point numbers
     function _preciseMul(uint256 _a, uint256 _b) internal pure returns (uint256) {
         return (_a * _b) / 1 ether;
     }
--- src/FuseBedOracle.sol
+++ src/FuseBedOracle.sol
@@ -5,21 +5,37 @@ import { IBedOracle } from "./interfaces/IBedOracle.sol";
 import { IFToken } from "./interfaces/IFToken.sol";
 
 
+/// @title FuseBedOracle
+/// @author ncitron
+/// @notice Price oracle for BED on Fuse
+/// @dev Consumes prices from BedOracle but conforms to the correct interfaces for Fuse
 contract FuseBedOracle {
     
+    /// @notice The BedOracle contract
     IBedOracle public bedOracle;
+
+    /// @notice The BED token
     address public bed;
 
+    /// @notice Creates a new BED price oracle for Fuse
+    /// @param _bedOracle The generic BedOracle contract
+    /// @param _bed The BED token address
     constructor(IBedOracle _bedOracle, address _bed) {
         bedOracle = _bedOracle;
         bed = _bed;
     }
 
+    /// @notice Gets the price of BED in ETH
+    /// @param _underlying The undelrlying token to get the price of. In this case it is always BED
+    /// @return The current price of BED in ETH
     function price(address _underlying) external view returns (uint256) {
         require(_underlying == bed, "oracle not found");
         return bedOracle.getPrice();
     }
 
+    /// @notice Gets the price of BED in ETH
+    /// @param _fToken The fToken whose underlying to fetch the price of. In this case it is always fBED
+    /// @return The current price of BED in ETH
     function getUnderlyingPrice(IFToken _fToken) external view returns (uint256) {
         require(_fToken.underlying() == bed, "oracle not found");
         return bedOracle.getPrice();
