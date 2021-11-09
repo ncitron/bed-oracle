260a419bd600db5615c1ce85410c4090b8c71d55
ncitron <noah@jeff.org>
2021-11-08 23:57:15 -0500

    add fuse oracle to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
-ORACLE=$(dapp create BedOracle $BED $WBTC $WETH $DPI $BTC_FEED $DPI_FEED)
-$(dapp verify-contract src/BedOracle.sol:BedOracle $ORACLE $BED $WBTC $WETH $DPI $BTC_FEED $DPI_FEED)
+BED_ORACLE=$(dapp create BedOracle $BED $WBTC $WETH $DPI $BTC_FEED $DPI_FEED)
+! dapp verify-contract src/BedOracle.sol:BedOracle $BED_ORACLE $BED $WBTC $WETH $DPI $BTC_FEED $DPI_FEED
+
+FUSE_BED_ORACLE=$(dapp create FuseBedOracle $BED_ORACLE $BED)
+! dapp verify-contract src/FuseBedOracle.sol:FuseBedOracle $FUSE_BED_ORACLE $BED_ORACLE $BED
+
+echo "deployments complete"
+echo "BED Oracle: $BED_ORACLE"
+echo "Fuse BED Oracle: $FUSE_BED_ORACLE"
